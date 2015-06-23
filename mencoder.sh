1e88d9f711408b1c7ef7c718408d630f47e8e95b
mario alberto <graff_rasmus@hotmail.com>
2015-06-23 14:22:07 -0500

    mencoder mas comentarios

--- mencoder.sh
+++ mencoder.sh
@@ -1,2 +1,6 @@
 #esto es para convetir videos mkv a .avi
 mencoder Video.mkv -vf scale=720:304 -oac mp3lame -lameopts preset=128 -ovc xvid -xvidencopts bitrate=700 -ofps 25 -of avi -vobsubid 0 -o Video.avi
+
+
+# -vobsubid    es el numero del subtitulo
+# scale es     scale=720:304 es la resolucion
