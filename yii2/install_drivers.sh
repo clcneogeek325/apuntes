d70a52fab889fb96964c4794aefe61c35b66bd75
mario alberto <clcneogeek325@gmail.com>
2016-03-02 16:33:41 -0600

    yii2 apuntes de sql server dblib

--- yii2/install_drivers.sh
+++ yii2/install_drivers.sh
@@ -1,6 +1,7 @@
 sudo apt-get install php5-mysql
-sudo apt-get install php5-mysqlnd
-sudo apt-get install php5-mysqlnd-ms
 sudo apt-get install php5-sqlite
 sudo apt-get install php5-pgsql
 sudo apt-get install php5-sybase
+sudo apt-get install php5-odbc
+
+#=== YII2 ===== la conexion php a SQL server solo fucniona con "dblib" ;
