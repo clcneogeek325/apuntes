eb518784937e2a33855da386c24ba671f2707b0e
neogeek325 <clcneogeek@gmail.com>
2016-03-01 23:06:00 -0600

    Drivers de Yii2 framework.

---
+++ yii2/install_drivers.sh
@@ -0,0 +1,6 @@
+sudo apt-get install php5-mysql
+sudo apt-get install php5-mysqlnd
+sudo apt-get install php5-mysqlnd-ms
+sudo apt-get install php5-sqlite
+sudo apt-get install php5-pgsql
+sudo apt-get install php5-sybase
