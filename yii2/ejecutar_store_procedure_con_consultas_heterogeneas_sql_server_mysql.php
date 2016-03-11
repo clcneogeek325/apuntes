55c25e2536dd356e722c06b648a34c66a23c0971
NEO <graff_rasmus@hotmail.com>
2016-03-11 11:03:49 -0600

    Create ejecutar_store_procedure_con_consultas_heterogeneas_sql_server_mysql.php
    consultas heterogenes mysql y sql server

---
+++ yii2/ejecutar_store_procedure_con_consultas_heterogeneas_sql_server_mysql.php
@@ -0,0 +1,4 @@
+Yii::$app->db_mssql->createCommand("SET ANSI_NULLS ON")->execute();
+Yii::$app->db_mssql->createCommand("SET  ANSI_WARNINGS ON")->execute();
+//necesitamos las 2 lineas anteriores para poder ejecutar una consulta o store procedure con consultas heterogeneas
+$lista = Yii::$app->db_mssql->createCommand("SELECT *FROM openquery(DB_MYSQL,'select *from archivo')")->queryAll();
